2021ef3dc03bb293b4a4d6e33ae0f270c263a7a9
Simone Maurizio La Cava <simonemaurizio.lacava@gmail.com>
2020-05-25 13:07:35 +0200

    Wiki updated

--- Auxiliary/define_cases.m
+++ Auxiliary/define_cases.m
@@ -36,7 +36,7 @@ function cases = define_cases(dataPath, check, extension)
         cases(strncmp({cases.name}, '.', 1),:) = [];
         for i = 1:length(cases)
             case_name = strcat(dataPath, cases(i).name);
-            subject = search_cases(case_name, check, extensions);     
+            subject = search_cases(case_name, check, extensions, 0);     
             if isempty(subject)
                 toDelete = [toDelete; i];
             else
@@ -62,12 +62,18 @@ function cases = check_cases(cases, check)
 end
     
 
-function cases = search_cases(dataPath, check, extensions)
+function cases = search_cases(dataPath, check, extensions, check_single)
+    if nargin == 3
+        check_single = 1;
+    end
     cases = [];
     for e = 1:length(extensions)
         if isempty(cases)
             cases = check_cases(dir(fullfile(char_check(dataPath), ...
                 extensions{e})), check);
+            if check_single == 1
+                cases = single_file_detected(cases);
+            end
         else
             break;
         end
@@ -87,4 +93,52 @@ function extensions = define_extensions(extension)
         end
         extensions = [extension, extensions];
     end
+end
+
+
+function [cases] = single_file_detected(cases)
+    if max(size(cases)) == 1 && not(isempty(cases))
+        bgc = [1 1 1];
+        fgc = [0.067 0.118 0.424];
+        btn = [0.427 0.804 0.722];
+        answer = 'no';
+        msg = strcat("A single possible file named ", cases(1).name, ...
+            " has been detected. ", ...
+            "Should I ignore it and continue the search?");
+        f = figure;
+        set(f, 'Position', [200 350 400 200], 'Color', bgc, ...
+            'MenuBar', 'none', 'Name', 'Single file detected', ...
+            'Visible', 'off', 'NumberTitle', 'off');
+        ht = uicontrol('Style', 'text', 'Units', 'normalized', ...
+            'Position', [0.1 0.45 0.8 0.4], 'String', char(msg), ...
+            'FontUnits', 'normalized', 'FontSize', 0.2, ...
+            'BackgroundColor', bgc, 'ForegroundColor', 'k');
+        hok = uicontrol('Style', 'pushbutton', 'String', 'yes',...
+            'Units', 'normalized', 'Position', [0.2 0.05 0.25 0.2], ...
+            'Callback', {@ok_Callback}, 'BackgroundColor', btn, ...
+            'ForegroundColor', fgc); 
+        hno = uicontrol('Style', 'pushbutton', 'String', 'no',...
+            'Units', 'normalized', 'Position', [0.6 0.05 0.25 0.2], ...
+            'Callback', {@no_Callback}, 'BackgroundColor', btn, ...
+            'ForegroundColor', fgc); 
+        hbar = uicontrol('Style', 'text', 'Units', 'normalized', ...
+            'Position', [0 0.985 0.2 0.015], 'String', '', ...
+            'FontUnits', 'normalized', ...
+            'BackgroundColor', fgc, 'ForegroundColor', 'k');
+        movegui(f, 'center')
+        set(f, 'Visible', 'on')
+        waitfor(hno)
+        if strcmp(answer, 'yes')
+            cases = [];
+        end
+    end
+end
+
+function ok_Callback(hObject, eventdata)
+    evalin('caller', "answer = 'yes';");
+    evalin('caller', 'close(f)')
+end
+
+function no_Callback(hObject, eventdata)
+    evalin('caller', 'close(f)')
 end
